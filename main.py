0cf6d5db123eb02cffe525df1419120041e1d420
dayahalibot <dayahali783@gmail.com>
2025-07-01 17:11:58 +0300

    dayahbotss

--- main.py
+++ main.py
@@ -1,94 +1,57 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.utils import executor
 
-import pyrogram, os, asyncio
+# BOT TOKEN KAAGA HALKAN KU DAR
+API_TOKEN = '7132920778:AAEOZTFz4y7NcHWi-w6Xx4k1-v9g8U4ZdYQ'
 
-app_id = int(os.environ.get("app_id", ""))
-api_hash = os.environ.get("api_hash", "")
-bot_token = os.environ.get("bot_token", "")
-custom_caption = os.environ.get("custom_caption", "`{file_name}`\n\n**Powered By - @VJ_Botz**") # Here You Can Give Anything, if You Want Real File Name Then Use {file_name}
+# Log setting
+logging.basicConfig(level=logging.INFO)
 
-AutoCaptionBotV1 = pyrogram.Client(name="AutoCaptionBotV1", api_id=app_id, api_hash=api_hash, bot_token=bot_token)
+# Bot & Dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
-start_message = """
-<b>👋Hello {}</b>
-<b>I am an AutoCaption bot</b>
-<b>All you have to do is to add me to your channel as admin and I will show you my power</b>
-<b>@VJ_Botz</b>"""
+# Dictionary si loo xafido tirada videos ka ee user walba
+user_video_counts = {}
 
-about_message = """
-<b>• Name : <a href=https://t.me/VJ_Botz>VJ AutoCaption</a></b>
-<b>• Developer : <a href=https://t.me/VJ_Botz>[VJ UPDATES]</a></b>
-<b>• Language : Python3</b>
-<b>• Library : Pyrogram v{version}</b>
-<b>• Updates : <a href=https://t.me/VJ_Botz>Click Here</a></b>
-<b>• Source Code : <a href=https://github.com/VJBots/VJ-AutoCaption-Bot>Click Here</a></b>"""
+@dp.message_handler(content_types=types.ContentType.VIDEO)
+async def handle_video(message: Message):
+    user_id = message.from_user.id
 
-@AutoCaptionBotV1.on_message(pyrogram.filters.private & pyrogram.filters.command(["start"]))
-def start_command(bot, update):
-    update.reply(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+    # Haddii uusan user-ka horay u jirin, bilaabi count 1
+    if user_id not in user_video_counts:
+        user_video_counts[user_id] = 1
+    else:
+        user_video_counts[user_id] += 1
 
-@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("start"))
-def strat_callback(bot, update):
-    update.message.edit(start_message.format(update.from_user.mention), reply_markup=start_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+    count = user_video_counts[user_id]import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.utils import executor
 
-@AutoCaptionBotV1.on_callback_query(pyrogram.filters.regex("about"))
-def about_callback(bot, update): 
-    bot = bot.get_me()
-    update.message.edit(about_message.format(version=pyrogram.__version__, username=bot.mention), reply_markup=about_buttons(bot, update.message), parse_mode=pyrogram.enums.ParseMode.HTML, disable_web_page_preview=True)
+# BOT TOKEN KAAGA HALKAN KU DAR
+API_TOKEN = '7132920778:AAEOZTFz4y7NcHWi-w6Xx4k1-v9g8U4ZdYQ'
 
-@AutoCaptionBotV1.on_message(pyrogram.filters.channel)
-def edit_caption(bot, update: pyrogram.types.Message):
-    techvj, _ = get_file_details(update)
-    try:
-       try:
-           update.edit(custom_caption.format(file_name=techvj.file_name))
-       except pyrogram.errors.FloodWait as FloodWait:
-           asyncio.sleep(FloodWait.value)
-           update.edit(custom_caption.format(file_name=techvj.file_name))
-       except:
-          pass
-    except pyrogram.errors.MessageNotModified:
-        pass 
-    
-def get_file_details(update: pyrogram.types.Message):
-    if update.media:
-        for message_type in (
-            "photo",
-            "animation",
-            "audio",
-            "document",
-            "video",
-            "video_note",
-            "voice",
-            "sticker"
-        ):
-            obj = getattr(update, message_type)
-            if obj:
-                return obj, obj.file_id
+# Log setting
+logging.basicConfig(level=logging.INFO)
 
-def start_buttons(bot, update):
-    bot = bot.get_me()
-    buttons = [[
-        pyrogram.types.InlineKeyboardButton("Updates", url="t.me/VJ_Botz"),
-        pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
-    ],[
-        pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{bot.username}?startchannel=true")
-    ]]
-    return pyrogram.types.InlineKeyboardMarkup(buttons)
+# Bot & Dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
-def about_buttons(bot, update):
-    buttons = [[
-        pyrogram.types.InlineKeyboardButton("🏠 Back To Home 🏠", callback_data="start")
-    ]]
-    return pyrogram.types.InlineKeyboardMarkup(buttons)
+# Dictionary si loo xafido tirada videos ka ee user walba
+user_video_counts = {}
 
-print("Telegram AutoCaption V1 Bot Start")
-print("Bot Created By @VJ_Botz")
+@dp.message_handler(content_types=types.ContentType.VIDEO)
+async def handle_video(message: Message):
+    user_id = message.from_user.id
 
-AutoCaptionBotV1.run()
+    # Haddii uusan user-ka horay u jirin, bilaabi count 1
+    if user_id not in user_video_counts:
+        user_video_counts[user_id] = 1
+    else:
+        user_video_counts[user_id] += 1
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+    count = user_video_counts[user_id]
